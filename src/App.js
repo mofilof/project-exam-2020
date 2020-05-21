7ce409a71020b258d3fe554d0a4c59fcdbae5c32
mofilof <44873229+mofilof@users.noreply.github.com>
2020-05-21 22:17:04 +0200

    App.js Routs
    Made some routs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 import Home from './components/home/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from './components/nav/Menu';
+import Home from './components/home/Home';
+import Contact from './components/contact/Contact';
 
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <h6>App.js</h6>
       <Home></Home>
+
+      <Nav />
+      <div>
+        <Router>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/home" component={Home} />
+            <Route path="/contact" component={Contact} />
+          </Switch>
+        </Router>
+      </div>
+
     </>
   );
 }
 
 export default App;
+
