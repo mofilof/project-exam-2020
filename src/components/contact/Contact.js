f5bc2f5ce9cdd63d975ca2b292ef7c08245bd572
mofilof <44873229+mofilof@users.noreply.github.com>
2020-05-25 01:07:41 +0200

    contact form
    Contact form with validation

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,14 +1,48 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+
+const schema = yup.object().shape({
+  fullName: yup.string().required("Your name is required.")
+    .min(2, "Minimum two characters"),
+  email: yup
+    .string()
+    .email("Enter valid email.")
+    .required("We need your email."),
+  textMessage: yup.string().required("Ops, you forgot to write a message.")
+    .min(10, "Minimum ten characters.")
+    .max(100, "Maximum 100 characters."),
+});
 
 function Contact() {
+  const { register, handleSubmit, errors } = useForm({
+    validationSchema: schema
+  });
+
+  function onSubmit(data) {
+    console.log("data", data);
+  }
+
   return (
-    <body className="bodycolor">
+    <>
       <main>
-        <h6>Contact.js</h6>
+        <form className="col-sm-4" onSubmit={handleSubmit(onSubmit)}>
+
+          <input className="col" name="fullName" placeholder="Full name" ref={register()} />
+          {errors.fullName && <p className="contactError">{errors.fullName.message}</p>}
+
+          <input className="col" name="email" placeholder="Email" ref={register()} />
+          {errors.email && <p className="contactError">{errors.email.message}</p>}
+
+          <input className="messageArea col" name="textMsessage" placeholder="Message" ref={register()} />
+          {errors.textMessage && <p className="contactError">{errors.textMessage.message}</p>}
+
+          <input className="col" className="submitInput" type="submit" />
+        </form>
 
 
       </main>
-    </body>
+    </>
   );
 }
 
