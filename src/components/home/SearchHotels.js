f68124818a089c7c1f837ca4152700bb6836322b
mofilof <44873229+mofilof@users.noreply.github.com>
2020-05-28 14:32:04 +0200

    Trying to connect the search to the button
    I dont get a result yet

--- src/components/home/SearchHotels.js
+++ src/components/home/SearchHotels.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import PropTypes from "prop-types";
 
+
+
 function SearchHotels({ doSearch }) {
+
   return (
     <>
-      <input className="searchbar"
-        placeholder="Search for hotels..."
-        onChange={function (e) {
-          console.log(e.target.value);
-          doSearch(e.target.value);
-        }}
-      />
-      <button className="search">Search</button>
+      <input className="searchbar" placeholder="Search hotels..." />
+
+      <button onClick={function (hotelResult) {
+        console.log(hotelResult.target.value);
+        doSearch(hotelResult.target.value);
+      }} id="searchresult" className="search">Search2.0</button>
+
     </>
   );
 }
@@ -21,3 +23,32 @@ SearchHotels.propTypes = {
 };
 
 export default SearchHotels;
+
+/*
+      <input className="searchbar"
+        placeholder="Search for hotels..."
+        onChange={function (e) {
+          console.log(e.target.value);
+          doSearch(e.target.value);
+        }}
+      />
+      <button id="searchresult" className="search">Search</button>
+
+*/
+
+
+
+/*
+function App() {
+
+  function sayHello() {
+    alert('Hello!');
+  }
+
+  return (
+    <button onClick={sayHello}>
+      Click me!
+    </button>
+  );
+}
+*/
