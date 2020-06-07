ea024216523daacea5879e7af45de6f483729b34
mofilof <44873229+mofilof@users.noreply.github.com>
2020-06-08 00:55:30 +0200

    ContactData.js gives me errors
    Dont see why

--- src/components/nav/AdminUser.js
+++ src/components/nav/AdminUser.js
@@ -11,12 +11,15 @@ function AdminUser() {
   /*Use sessionStorage? Store the form during the session */
   /*Is it possible to use sessionStorage to store and delete forms during the session? Perhaps also a possible solution for the booking page?? */
 
+
   return (
     <main>
       <h1>Welcome {user}</h1>
 
       <h1>Contact</h1>
       <h3>Name, email and messages from customers:</h3>
+
+      {/*<ContactData>*  Why do I det error because of this file? Is it restricted words?*/}
 
       <h2>Message from {formName}</h2>
       <p><b>Name:</b> {formName}</p>
